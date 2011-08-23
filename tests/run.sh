c617e18c1c829d014b7d8e9d172fdad09603ed99
ezku <eevert.saukkokoski@brainalliance.com>
2011-08-23 13:28:06 +0300

    added README and test bootstraps

---
+++ tests/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# default binary location (from Zend Server)
+phpunit=/usr/local/zend/bin/phpunit
+
+# possible to use a custom location using an environment variable
+if [ -n "$PHPUNIT" ]; then
+    phpunit="$PHPUNIT"
+fi
+
+# run the tests, passing all given arguments to phpunit
+cd `dirname "$0"`
+
+exec "$phpunit" \
+        --bootstrap bootstrap.php \
+        --configuration phpunit.xml \
+        "$@"
