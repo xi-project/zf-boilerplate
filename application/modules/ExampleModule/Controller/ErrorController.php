511eafe3ce707e088cb059d122b5643ffc4fdb4a
Martin Pärtel <martin.partel@gmail.com>
2011-08-29 15:53:13 +0300

    A minimal error controller and view.

--- application/modules/ExampleModule/Controller/ErrorController.php
+++ application/modules/ExampleModule/Controller/ErrorController.php
@@ -5,6 +5,8 @@ class ErrorController extends \Zend_Controller_Action
 {
     public function errorAction()
     {
-        
+        if ($this->getFrontController()->getParam('displayExceptions')) {
+            $this->view->exception = $this->_getParam('error_handler')->exception;
+        }
     }
 }
