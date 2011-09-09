69920f43899b3142923607dfd0c6a3cbae56c3c4
Martin Pärtel <martin.partel@gmail.com>
2011-09-09 10:29:40 +0300

    (oops)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-define('APPLICATION_ENV', 'development');
+define('APPLICATION_ENV', 'testing');
 require dirname(__DIR__) . '/scripts/bootstrap.php';
 
 $application->getAutoloader()->pushAutoloader(array(new \Xi\Zend\Application\ClassLoader(null, __DIR__ . '/library'), 'loadClass'));
